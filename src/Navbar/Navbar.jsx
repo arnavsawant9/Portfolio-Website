6f51d1f190bff605704ef3f98f64d0f0e9b4be20
arnavsawant9 <arnav.sawantbhosale@gmail.com>
2025-02-16 18:04:49 +0530

    navbar done

---
+++ src/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import taylor from '../assets/taylor.webp'
+
+const Navbar = () => {
+  return (
+    <div className='p-4 bg-gray-800 flex w-full justify-center'>
+      {/* <div className='h-10 w-10 rounded-full'>
+        <img src={taylor} />
+      </div> */}
+      <div className='flex justify-center'>
+        <button className='text-lg text-white px-6 hover:text-red-500'>Home</button>
+        <button className='text-lg text-white px-6 hover:text-red-500'>About Me</button>
+        <button className='text-lg text-white px-6 hover:text-red-500'>Projects</button>
+        <button className='text-lg text-white px-6 hover:text-red-500'>Contact</button>
+        <button className='text-lg text-white px-6 hover:text-red-500'>Resume</button>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
